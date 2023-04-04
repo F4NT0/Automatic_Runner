c6dd912190c11b0a4d339491b8ac4b1cdfc7f1b2
Stundner, Gabriel <gabriel.stundner@dell.com>
2023-04-04 11:43:08 -0300

    #2 Criado bat file e seu runner

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running runner.bat...
+call runner.bat
+echo runner.bat finished!
---
+++ runner.bat
@@ -0,0 +1,3 @@
+@echo off
+start notepad.exe
+python C:\Users\Gabriel_Stundner\Downloads\GIT\Automatic_Runner\pyrunner.py
